488acca8a9077d20887679b3768730cf0269acf4
Lorrane Alves <Lorrane.dev@gmail.com>
2021-07-12 16:28:38 -0300

    Implementando update

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,22 +18,28 @@ public class Program {
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
-        System.out.println("==== TEST 2: Seller findByDepartment ====");
+        System.out.println("\n==== TEST 2: Seller findByDepartment ====");
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
         for (Seller obj: list) {
             System.out.println(obj);
         }
 
-        System.out.println("==== TEST 3: Seller findAll ====");
+        System.out.println("\n==== TEST 3: Seller findAll ====");
         list = sellerDao.findAll();
         for (Seller obj: list) {
             System.out.println(obj);
         }
 
-        System.out.println("==== TEST 4: Seller insert ====");
+        System.out.println("\n==== TEST 4: Seller insert ====");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n==== TEST 5: Seller update ====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
