6748252af9089261a8a56887fdec282f8be8fb6f
Lorrane Alves <Lorrane.dev@gmail.com>
2021-07-12 16:40:04 -0300

    Implementando delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,11 +7,13 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("==== TEST 1: Seller findById ====");
@@ -40,6 +42,13 @@ public class Program {
         seller = sellerDao.findById(1);
         seller.setName("Martha Waine");
         sellerDao.update(seller);
-        System.out.println("Update completed");
+
+        System.out.println("\n==== TEST 6: Seller delete ====");
+        System.out.print("Enter with an id to delete: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+
+        sc.close();
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -82,7 +82,21 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void deleteById(Integer id) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
 
+            st.setInt(1, id);
+            int rows = st.executeUpdate();
+
+            if (rows == 0){
+                throw new DbException("Id não existe no banco! Nenhum seller foi excluído.");
+            }
+        } catch (SQLException e){
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
